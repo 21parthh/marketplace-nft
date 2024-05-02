baf6e10b6d81c8a27d736338049f407fa907abfd
Parth Deore <parth.deore@icloud.com>
2024-05-02 19:57:43 +0530

    error solved

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import Header from '../components/Header';
 import Hero from '../components/Hero';
 
 export default function Home() {
-  return 
+  return (
   <>
     <Header />
     <Hero />
   </>
-
+  )
 }
