98acf23df8f9e9e9e48b45b4f14725be47e0d81d
Ishita Patil <ishitapatil088@gmail.com>
2024-05-02 17:11:37 +0530

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
-import Header from "../components/header";
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+
 export default function Home() {
-  return Header
-  
+  return 
+  <>
+    <Header />
+    <Hero />
+  </>
 
 }
