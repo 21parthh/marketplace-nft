550996c4883e80fe64eb3641a1fbc5d9022f50d9
Parth Deore <parth.deore@icloud.com>
2024-05-06 02:06:11 +0530

    Sanity Auth Data Collected in Database

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,25 @@ import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import { useEffect } from "react";
+import { client } from "../lib/sanityClient";
 
 export default function Home() {
   const address = useAddress();
+
+  useEffect(() => {
+    if(!address) return
+      ;(async () => {
+        const userDoc = {
+          _type:'users',
+        _id: address,
+        userName: 'Unnamed',
+        walletAddress: address
+        }
+        const result = await client.createIfNotExists(userDoc)
+      })()
+  }, [address ])
+  
   return (
     <div>
       {address ? (
